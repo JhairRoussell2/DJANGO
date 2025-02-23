8eede3622aa8c980ce37026080e68b49e68dbcaf
JhairRoussell2 <jhairrmb3@gmail.com>
2025-02-23 02:46:24 -0500

    Vistas: de funcion a clase

--- my_first_project/my_first_app/views.py
+++ my_first_project/my_first_app/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 
@@ -11,3 +13,21 @@ def my_view(request):
         "car_list": car_list
     }
     return render(request, "my_first_app/car_list.html", context)
+
+class CarListView(TemplateView):
+    template_name = "my_first_app/car_list.html"
+
+    def get_context_data(self, **kwargs):
+        car_list = [
+                {"title": "BMW"},
+                {"title": "Mazda"},
+        ]
+        return {
+                "car_list": car_list
+        }
+
+
+def my_test_view(request, *args, **kwargs):
+    print(args)
+    print(kwargs)
+    return HttpResponse("")
